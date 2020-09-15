3389c86f6697563b61373b33ca8a89d976604db0
aihailin <1766447919@qq.com>
2020-09-15 13:50:12 +0800

    add. CircularLinkedList

---
+++ src/CircularLinkedList/CircularLinkedList.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The file is part of the data-structure.
+ *
+ * (c) alan <alan1766447919@gmail.com>.
+ *
+ * 2020/9/15 1:34 下午
+ */
+
+namespace Alan\Structure\CircularLinkedList;
+
+/**
+ * Doubly circular linked list.
+ * Class CircularLinkedList
+ * @package Alan\Structure\CircularLinkedList
+ */
+class CircularLinkedList
+{
+    // doubly
+
+    // add
+    // insert
+    // remove
+    // set
+    // get
+    // getDistance
+    // getLength
+
+    // ArrayAccess
+    // Iterator
+}
